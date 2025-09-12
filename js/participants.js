7d34a640e3bb810318a51032cddfb209ae25f8e6
zaenalarifin10 <siskondesuofficial@gmail.com>
2025-09-13 00:39:02 +0700

    Create participants.js

---
+++ js/participants.js
@@ -0,0 +1,7 @@
+const participantsData = [
+    { teamName: "ThunderBolt", leader: "Rizky", members: 4, status: "Terkonfirmasi" },
+    { teamName: "FireStorm", leader: "Andi", members: 4, status: "Terkonfirmasi" },
+    { teamName: "ShadowSquad", leader: "Budi", members: 4, status: "Terkonfirmasi" },
+    { teamName: "BlazeHunters", leader: "Siti", members: 4, status: "Terkonfirmasi" },
+    { teamName: "NightRiders", leader: "Dedi", members: 4, status: "Terkonfirmasi" },
+];
