d791c0f658aea4600230392cac838e7dbed86ffc
zaenalarifin10 <siskondesuofficial@gmail.com>
2025-09-13 01:18:13 +0700

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -1,7 +1,47 @@
 const scheduleData = [
-    { date: "15 September 2025", time: "14:00 WIB", team1: "ThunderBolt", team2: "FireStorm" },
-    { date: "15 September 2025", time: "15:30 WIB", team1: "ShadowSquad", team2: "BlazeHunters" },
-    { date: "15 September 2025", time: "17:00 WIB", team1: "NightRiders", team2: "ThunderBolt" },
-    { date: "16 September 2025", time: "14:00 WIB", team1: "FireStorm", team2: "ShadowSquad" },
-    { date: "16 September 2025", time: "15:30 WIB", team1: "BlazeHunters", team2: "NightRiders" },
+    { 
+        date: "15 September 2025", 
+        time: "14:00 WIB", 
+        team1: "ThunderBolt", 
+        team2: "FireStorm", 
+        status: "selesai", 
+        score: { team1: 45, team2: 30 }, 
+        winner: "ThunderBolt" 
+    },
+    { 
+        date: "15 September 2025", 
+        time: "15:30 WIB", 
+        team1: "ShadowSquad", 
+        team2: "BlazeHunters", 
+        status: "selesai", 
+        score: { team1: 20, team2: 50 }, 
+        winner: "BlazeHunters" 
+    },
+    { 
+        date: "15 September 2025", 
+        time: "17:00 WIB", 
+        team1: "NightRiders", 
+        team2: "ThunderBolt", 
+        status: "sedang berlangsung", 
+        score: { team1: 0, team2: 0 }, 
+        winner: null 
+    },
+    { 
+        date: "16 September 2025", 
+        time: "14:00 WIB", 
+        team1: "FireStorm", 
+        team2: "ShadowSquad", 
+        status: "belum mulai", 
+        score: { team1: 0, team2: 0 }, 
+        winner: null 
+    },
+    { 
+        date: "16 September 2025", 
+        time: "15:30 WIB", 
+        team1: "BlazeHunters", 
+        team2: "NightRiders", 
+        status: "belum mulai", 
+        score: { team1: 0, team2: 0 }, 
+        winner: null 
+    },
 ];
