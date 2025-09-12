99e7f2c45dc03a32c735b7d349c98c24a4658196
zaenalarifin10 <siskondesuofficial@gmail.com>
2025-09-13 00:54:28 +0700

    Create schedule.js

---
+++ js/schedule.js
@@ -0,0 +1,7 @@
+const scheduleData = [
+    { date: "15 September 2025", time: "14:00 WIB", team1: "ThunderBolt", team2: "FireStorm" },
+    { date: "15 September 2025", time: "15:30 WIB", team1: "ShadowSquad", team2: "BlazeHunters" },
+    { date: "15 September 2025", time: "17:00 WIB", team1: "NightRiders", team2: "ThunderBolt" },
+    { date: "16 September 2025", time: "14:00 WIB", team1: "FireStorm", team2: "ShadowSquad" },
+    { date: "16 September 2025", time: "15:30 WIB", team1: "BlazeHunters", team2: "NightRiders" },
+];
